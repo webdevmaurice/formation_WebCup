e4a9261fbc8e2d3dba2aac496c2812287f5396d3
Rajendra.ballgobin <rajendra.ballgobin@pharmagest.mu>
2016-03-30 15:57:55 +0400

    test

--- client/app/main_component/voyage/VoyageCtrl.js
+++ client/app/main_component/voyage/VoyageCtrl.js
@@ -2,6 +2,6 @@
  * Created by ballgobina on 28/3/2016.
  */
 angular.module('teleportation')
-    .controller('VoyageCtrl',[function VoyageCtrl () {
+    .controller('VoyageCtrl',['$window', function VoyageCtrl ($window) {
         var vm = this;
     }]);
