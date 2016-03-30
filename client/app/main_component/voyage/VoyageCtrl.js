ffd41142636ee506e59327f83958d8c0567c549d
ssohomow <saif.sohomow@pharmagest.com>
2016-03-30 14:56:52 +0400

    Typo correction

--- client/app/main_component/voyage/VoyageCtrl.js
+++ client/app/main_component/voyage/VoyageCtrl.js
@@ -2,6 +2,6 @@
  * Created by ballgobina on 28/3/2016.
  */
 angular.module('teleportation')
-    .controller('VoyageCtrl',[function MyAccountCtrl () {
+    .controller('VoyageCtrl',[function VoyageCtrl () {
         var vm = this;
     }]);
