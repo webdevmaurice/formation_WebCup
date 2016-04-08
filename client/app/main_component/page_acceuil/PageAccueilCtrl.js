47646d186a6d0224b71e5448bcac004d5acae383
mveerapen <m.veerapenus@gmail.com>
2016-04-08 23:28:14 +0400

    Michael g

--- client/app/main_component/page_acceuil/PageAccueilCtrl.js
+++ client/app/main_component/page_acceuil/PageAccueilCtrl.js
@@ -7,3 +7,17 @@ angular.module('teleportation')
 
 
 
+window.onload = function() {
+    jQuery('#page_home').vegas({
+        slides: [
+            { src: 'img/slide1.jpg' },
+            { src: 'img/slide2.jpg' },
+            { src: 'img/slide3.jpg' }
+        ],
+        delay: 7000,
+        timer: false,
+        shuffle: true,
+        transition: 'slideDown2',
+        transitionDuration: 2000
+    });
+}(jQuery);
