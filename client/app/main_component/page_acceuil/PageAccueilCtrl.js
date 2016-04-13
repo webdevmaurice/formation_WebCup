3f56c2ea4fa9b941116f5f1a073b400fc2842ff8
ssohomow <saif.sohomow@pharmagest.com>
2016-04-13 17:02:58 +0400

    Typewrite + housekeeping

--- client/app/main_component/page_acceuil/PageAccueilCtrl.js
+++ client/app/main_component/page_acceuil/PageAccueilCtrl.js
@@ -4,13 +4,11 @@ angular.module('teleportation')
         var vm = this;
         vm.pageClass = "page-acceuil";
 
-        var root = GetSiteRoot();
-
         $("#accueil").vegas({
             slides: [
-                { src: root+'/client/app/main_component/page_acceuil/img/slide1.jpg',transition:'flash2'},
-                { src: root+'/client/app/main_component/page_acceuil/img/slide2.jpg',transition:'swirlRight2'},
-                { src: root+'/client/app/main_component/page_acceuil/img/slide3.jpg',transition:'flash'}
+                { src: 'client/app/main_component/page_acceuil/img/slide1.jpg',transition:'flash2'},
+                { src: 'client/app/main_component/page_acceuil/img/slide2.jpg',transition:'swirlRight2'},
+                { src: 'client/app/main_component/page_acceuil/img/slide3.jpg',transition:'flash'}
             ],
             delay: 6000,
             timer: false,
@@ -18,23 +16,3 @@ angular.module('teleportation')
             transitionDuration: 15000
         });
     }]);
-
-
-function GetSiteRoot()
-{
-    var rootPath = window.location.protocol + "//" + window.location.host + "/";
-    if (window.location.hostname == "localhost")
-    {
-        var path = window.location.pathname;
-        if (path.indexOf("/") == 0)
-        {
-            path = path.substring(1);
-        }
-        path = path.split("/", 1);
-        if (path != "")
-        {
-            rootPath = rootPath + path + "/";
-        }
-    }
-    return rootPath;
-}
