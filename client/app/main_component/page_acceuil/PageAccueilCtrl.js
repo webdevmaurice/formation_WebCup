6286b53d82df5d0bfd6c5c90f79e25a33ffc411b
mveerapen <m.veerapenus@gmail.com>
2016-04-19 00:04:51 +0400

    About us

--- client/app/main_component/page_acceuil/PageAccueilCtrl.js
+++ client/app/main_component/page_acceuil/PageAccueilCtrl.js
@@ -7,8 +7,8 @@ angular.module('teleportation')
         $("#accueil").vegas({
             slides: [
                 { src: 'client/app/main_component/page_acceuil/img/slide1.jpg',transition:'zoomOut'},
-                { src: 'client/app/main_component/page_acceuil/img/slide2.jpg',transition:'swirlRight2'},
-                { src: 'client/app/main_component/page_acceuil/img/slide3.jpg',transition:'zoomOut2'}
+                { src: 'client/app/main_component/page_acceuil/img/slide2.jpg',transition:'zoomOut'},
+                { src: 'client/app/main_component/page_acceuil/img/slide3.jpg',transition:'zoomOut'}
             ],
             delay: 6000,
             timer: false,
