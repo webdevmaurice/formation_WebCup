d51fe8a6bb5339bc08f534016f8bce8997842e60
ssohomow <saif.sohomow@pharmagest.com>
2016-04-19 17:31:40 +0400

    Scroll Item

--- client/app/shared_component/nav/navCtrl.js
+++ client/app/shared_component/nav/navCtrl.js
@@ -1,5 +1,5 @@
 angular.module('teleportation')
-.controller('navCtrl',['$scope',, function navCtrl($scope){
+.controller('navCtrl',['$scope', function navCtrl($scope){
     var nav = this;
     nav.menuIsOpen = false;
     nav.menuIcon = 'menu';
