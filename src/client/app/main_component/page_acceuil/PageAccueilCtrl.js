cf39e0dbc1c42e09aa107d5229e0050fe5b49cae
mveerapen <m.veerapenus@gmail.com>
2016-05-11 00:52:16 +0400

    Layout template page d'accueil

--- src/client/app/main_component/page_acceuil/PageAccueilCtrl.js
+++ src/client/app/main_component/page_acceuil/PageAccueilCtrl.js
@@ -6,9 +6,9 @@ angular.module('teleportation')
 
         $("#accueil").vegas({
             slides: [
-                { src: 'client/app/main_component/page_acceuil/img/slide1.jpg',transition:'zoomOut'},
-                { src: 'client/app/main_component/page_acceuil/img/slide2.jpg',transition:'zoomOut'},
-                { src: 'client/app/main_component/page_acceuil/img/slide3.jpg',transition:'zoomOut'}
+                { src: 'client/app/main_component/page_acceuil/img/gif2.gif',transition:'zoomOut'},
+                { src: 'client/app/main_component/page_acceuil/img/gif3.gif',transition:'zoomOut'},
+                { src: 'client/app/main_component/page_acceuil/img/gif1.gif',transition:'zoomOut'}
             ],
             delay: 8000,
             timer: false,
